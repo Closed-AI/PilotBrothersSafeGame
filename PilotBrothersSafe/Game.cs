cb6e1da52b57f9efbb5bdc96d4fbd4abf9a7bb93
ARS <closedAI@mail.ru>
2022-10-01 14:51:59 +0300

    Game class

---
+++ PilotBrothersSafe/Game.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PilotBrothersSafe
+{
+    internal class Game
+    {
+        private Button[][] _desk;
+
+        public Button[][] Desk { get => _desk; }
+
+        public Game(int deskSize, double deskSizeInWindow,RoutedEventHandler deskButtonClick)
+        {
+            _desk = new Button[deskSize][];
+            double size = deskSizeInWindow / deskSize;
+
+            for (int i = 0; i < deskSize; i++)
+            {
+                _desk[i] = new Button[deskSize];
+
+                for (int j = 0; j < deskSize; j++)
+                {
+                    var button = new Button();
+                    button.Width    = size;
+                    button.Height   = size;
+                    button.FontSize = size;
+                    button.Content = "-";
+                    button.Click += deskButtonClick;
+                    _desk[i][j] = button;
+                }
+            }
+        }
+
+        public void Step(int x, int y)
+        {
+            for (int id = 0; id < _desk.Length; id++)
+            {
+                _desk[id][y].Content = _desk[id][y].Content.ToString() == "-" ? "|" : "-";
+
+                if (id != y)
+                    _desk[x][id].Content = _desk[x][id].Content.ToString() == "-" ? "|" : "-";
+            }
+        }
+
+        public void Refresh(int steps)
+        {
+            int x;
+            int y;
+
+            Random rand = new Random();
+
+            do
+            {
+                for (int i = 0; i < steps; i++)
+                {
+                    x = rand.Next(_desk.Length);
+                    y = rand.Next(_desk.Length);
+
+                    Step(x, y);
+                }
+            }
+            while (Win());
+        }
+
+        public bool Win()
+        {
+            string sumbol = (string)_desk[0][0].Content;
+
+            for (int i = 0; i < _desk.Length; i++)
+                foreach (var el in _desk[i])
+                    if ((string)el.Content != sumbol)
+                    {
+                        return false;
+                    }
+
+            return true;
+        }
+    }
+}
--- PilotBrothersSafe/MainWindow.xaml.cs
+++ PilotBrothersSafe/MainWindow.xaml.cs
@@ -1,53 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PilotBrothersSafe
 {
     public partial class MainWindow : Window
     {
-        private Button[][] _buttons;
+        private Game _game;
 
         public MainWindow()
         {
             InitializeComponent();
             InitDesk(5);
         }
-
-        private Button[] CreateButtons(int quantity)
-        {
-            double size = 600d / quantity;
-            Button[] buttons = new Button[quantity];
-            for (int i = 0; i < buttons.Length; i++)
-            {
-                buttons[i] = new Button();
-                buttons[i].Width = size;
-                buttons[i].Height = size;
-                buttons[i].Content = "-";
-                buttons[i].FontSize = size;
-                buttons[i].Click += FieldButton_Click;
-            }
-            return buttons;
-        }
-
-        private void AddToWrapPanel(Button[] buttons)
-        {
-            for (int i = 0; i < buttons.Length; i++)
-                wrapPanel.Children.Add(buttons[i]);
-        }
-
-        private int GetQuantityButtons()
+        
+        private int GetDeskSize()
         {
             int count = -1;
             try
@@ -64,7 +31,7 @@ namespace PilotBrothersSafe
 
         private void СreateButton_Click(object sender, RoutedEventArgs e)
         {
-            int size = GetQuantityButtons();
+            int size = GetDeskSize();
 
             if (size >= 2 && size <= 20)
                 InitDesk(size);
@@ -74,7 +41,7 @@ namespace PilotBrothersSafe
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e)
         {
-            Refresh(100);
+            _game.Refresh(100);
         }
 
         private void InitDesk(int size)
@@ -82,86 +49,39 @@ namespace PilotBrothersSafe
             if (wrapPanel.Children.Count > 0)
                 wrapPanel.Children.Clear();
 
+            _game = new Game(size, wrapPanel.Width, FieldButton_Click);
+            
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    wrapPanel.Children.Add(_game.Desk[i][j]);
 
-            _buttons = new Button[size][];
-
-            for (int i = 0; i < _buttons.Length; i++)
-            {
-                _buttons[i] = CreateButtons(size);
-                AddToWrapPanel(_buttons[i]);
-            }
-
-            Refresh(100);
+            _game.Refresh(100);
 
             wrapPanel.UpdateLayout();
         }
 
-        private void Refresh(int steps)
-        {
-            int x;
-            int y;
-
-            Random rand = new Random();
-
-            do
-            {
-                for (int i = 0; i < steps; i++)
-                {
-                    x = rand.Next(_buttons.Length);
-                    y = rand.Next(_buttons.Length);
-
-                    Step(x, y);
-                }
-            }
-            while (Win());
-        }
-
-        private void FieldButton_Click(object sender, RoutedEventArgs e)
+        public void FieldButton_Click(object sender, RoutedEventArgs e)
         {
             int x = 0;
             int y = 0;
 
-            for (int i = 0; i < _buttons.Length; i++)
-                for (int j = 0; j < _buttons.Length; j++)
-                    if ((Button)sender == _buttons[i][j])
+            for (int i = 0; i < _game.Desk.Length; i++)
+                for (int j = 0; j < _game.Desk.Length; j++)
+                    if ((Button)sender == _game.Desk[i][j])
                     {
                         x = i;
                         y = j;
                     }
 
-            Step(x, y);
+            _game.Step(x, y);
 
             wrapPanel.UpdateLayout();
 
-            if (Win())
+            if (_game.Win())
             {
                 MessageBox.Show("Вы выиграли!!!");
             }
         }
 
-        private void Step(int x, int y)
-        {
-            for (int id = 0; id < _buttons.Length; id++)
-            {
-                _buttons[id][y].Content = _buttons[id][y].Content.ToString() == "-" ? "|" : "-";
-
-                if (id != y)
-                    _buttons[x][id].Content = _buttons[x][id].Content.ToString() == "-" ? "|" : "-";
-            }
-        }
-
-        private bool Win()
-        {
-            string sumbol = (string)_buttons[0][0].Content;
-
-            for (int i = 0; i < _buttons.Length; i++)
-                foreach (var el in _buttons[i])
-                    if ((string)el.Content != sumbol)
-                    {
-                        return false;
-                    }
-
-            return true;
-        }
     }
 }
